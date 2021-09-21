6717fa0ae8a80fda7c792e9ebdfa2c3b8d6e77fb
FredericoHallamman <90877284+FredericoFreitas@users.noreply.github.com>
2021-09-21 02:16:41 +0100

    criado estrutura do ordenado

--- carteira GITHUB.py
+++ carteira GITHUB.py
@@ -1,8 +1,40 @@
-nome = str(input('Digite seu nome: ')).title().strip()
+nome = str(input('Óla, Digite seu nome: ')).title().strip()
+cores = {'roxo':'\033[1;35m', 'azul':'\033[;34m', 'red':'\033[1;31m', 'fim':'\033[m'}
+print('Hello {}{}{}!!! '.format(cores['roxo'], nome, cores['fim']))
+print('A partir de agora vou te auxiliar na sua gestão financeira ok.')
 
-cores = {'vermelho':'\033[1;31m', 'limpa':'\033[m'}
-print('Olá {}{}{} Prazer te conhecer !!!'.format(cores['vermelho'], nome, cores['limpa']))
-print('Vou te auxiliar na sua gestão financeira  vamos começar !!!')
+ordenado = float(input('\nDigite o valor do seu ordenado: '))
+print('\n{}OBS: !!!{}''Em portugal nomalmente a conta de luz e gás são juntas e outras separas.'.format(cores['red'], cores['fim']))
+gaseluz = str(input('A sua fatura é junta? s ou n ? ')).strip()
+
+if gaseluz == 's':
+
+    luzegas = float(input('\nDigite o valor total da sua fatura: '))
+    r = luzegas
+else:
+    luz = float(input('\nDigite o valor da fatura de Luz: '))
+    gas = float(input('Digite o valor que costuma comprar o gás: '))
+    r = luz+gas
+água = float(input('Digite o valor da fatura de água: '))
+renda = float(input('Digite o valor da renda: '))
 
 
-# EM CONSTRUÇÃO ... 
+internet = str(input('\nVoçê possui algum plano de internet em casa? s ou n? ')).strip()
+if internet == 's':
+    net = float(input('Digite o valor da fatura: '))
+else:
+    print('ok')
+
+if gaseluz == 's':
+    dispesa = (ordenado - luzegas - água)
+
+else:
+    dispesa = (ordenado - luz - gas - água)
+
+
+if internet == 's':
+    dispesa = (ordenado - r - água - net - renda)
+    print('\nO valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás, agua e internet é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],dispesa,cores['fim']))
+else:
+    dispesa = (ordenado - r - água - renda)
+    print('O valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás e agua é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],dispesa,cores['fim']))
