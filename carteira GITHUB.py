1858f42f24dd685c243e0232bda8b3f13e361f12
FredericoHallamman <90877284+FredericoFreitas@users.noreply.github.com>
2021-09-21 22:54:25 +0100

    Update carteira GITHUB.py

--- carteira GITHUB.py
+++ carteira GITHUB.py
@@ -4,7 +4,7 @@ print('Hello {}{}{}!!! '.format(cores['roxo'], nome, cores['fim']))
 print('A partir de agora vou te auxiliar na sua gestão financeira ok.')
 
 ordenado = float(input('\nDigite o valor do seu ordenado: '))
-print('\n{}OBS: !!!{}''Em portugal nomalmente a conta de luz e gás são juntas e outras separas.'.format(cores['red'], cores['fim']))
+print('\n{}OBS: !!!{}''Em portugal normalmente a conta de luz e gás são juntas e outras separas.'.format(cores['red'], cores['fim']))
 gaseluz = str(input('A sua fatura é junta? s ou n ? ')).strip()
 
 if gaseluz == 's':
@@ -23,7 +23,7 @@ internet = str(input('\nVoçê possui algum plano de internet em casa? s ou n ? 
 if internet == 's':
     net = float(input('Digite o valor da fatura: '))
 else:
-    print('ok')
+    print('')
 
 if gaseluz == 's':
     dispesa = (ordenado - luzegas - água)
@@ -35,11 +35,11 @@ else:
 if internet == 's':
     dispesa = (ordenado - r - água - net - renda)
     x = dispesa
-    print('\nO valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás, agua e internet é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],x,cores['fim']))
+    print('\nO valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás, água e internet é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],x,cores['fim']))
 else:
     dispesa = (ordenado - r - água - renda)
     x = dispesa
-    print('O valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás e agua é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],x,cores['fim']))
+    print('O valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás e água é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],x,cores['fim']))
 
 açao = str(input('\nVai querer adicionar mais alguma dispesa? s ou n ? ')).strip()
 if açao == 's':
@@ -49,7 +49,7 @@ if açao == 's':
     y = x - valor
     print('Foi retirado: ${}{:.2f}{}, para {}{}{}, restando na carteira o valor de: ${}{:.2f}{}'.format(cores['azul'],valor,cores['fim'],cores['verde'],nome,cores['fim'],cores['azul'],y,cores['fim']))
 else:
-    print('ok, {}{}{}: temos na carteira valor total de: ${}{:.2f}{}'.format(cores['roxo'],nome,cores['fim'],cores['azul'],x,cores['fim']))
+    print('\nok, {}{}{}: temos na carteira valor total de: ${}{:.2f}{}'.format(cores['roxo'],nome,cores['fim'],cores['azul'],x,cores['fim']))
 
 
-print('\33[7;30;41mPrograma inda em desenvolvimento\... \33[m')
+print('\33[7;30;41mPrograma ainda em desenvolvimento\... \33[m')
