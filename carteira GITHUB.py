37761ccfbb1a9ae674fd2487b463514427e9f8b2
Frederico Hallamman <fredericohallamman@gmail.com>
2021-11-30 23:14:59 +0000

    finalizado processo de porcentagem

--- carteira GITHUB.py
+++ carteira GITHUB.py
@@ -11,21 +11,28 @@ if gaseluz == 's':
 
     luzegas = float(input('\nDigite o valor total da sua fatura: '))
     r = luzegas
-    porcentagem = 100*r/ordenado
-    print('A porcentagem da dispesa de luz e gás é de: {:.2f}%'.format(porcentagem))
+    porcentagem = 100 * r / ordenado
+    print('A porcentagem da dispesa de luz e gás é de: {:.2f} %'.format(porcentagem))
 else:
     luz = float(input('\nDigite o valor da fatura de Luz: '))
     gas = float(input('Digite o valor que costuma comprar o gás: '))
     r = luz+gas
-    porcentagem = 100*r/ordenado
-    print('A porcentagem da dispesa de luz e gás é de: {:.2f}%'.format(porcentagem))
-água = float(input('Digite o valor da fatura de água: '))
-renda = float(input('Digite o valor da renda: '))
+    porcentagem = 100 * r / ordenado
+    print('A porcentagem da dispesa de luz e gás é de: {:.2f} %'.format(porcentagem))
 
+água = float(input('\nDigite o valor da fatura de água: '))
+porcentagem = 100 * água / ordenado
+print('A porcentagem da dispesa de água é de: {:.2f} %'.format(porcentagem))
+
+renda = float(input('\nDigite o valor da renda: '))
+porcentagem = 100 * renda / ordenado
+print('A porcentagem da dispesa de Aluguel é de: {:.2f} %'.format(porcentagem))
 
 internet = str(input('\nVoçê possui algum plano de internet em casa? s ou n ? ')).strip()
 if internet == 's':
     net = float(input('Digite o valor da fatura: '))
+    porcentagem = 100 * net / ordenado
+    print('A porcentagem da dispesa de internet é de: {:.2f} %'.format(porcentagem))
 else:
     print('')
 
@@ -39,19 +46,25 @@ else:
 if internet == 's':
     dispesa = (ordenado - r - água - net - renda)
     x = dispesa
+    porcentagem = 100 * x / ordenado
     print('\nO valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás, água e internet é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],x,cores['fim']))
+    print('A porcentagem total das dispesas é de: {:.2f} %'.format(porcentagem))
 else:
     dispesa = (ordenado - r - água - renda)
     x = dispesa
+    porcentagem = 100 * x / ordenado
     print('O valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás e água, aluguel é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],x,cores['fim']))
+    print('A porcentagem total das dispesas é de: {:.2f} %'.format(porcentagem))
 
 açao = str(input('\nVai querer adicionar mais alguma dispesa? s ou n ? ')).strip()
 if açao == 's':
     variavel = str(input('\nDigite o nome da da dispesa para registrar: ')).title().strip()
     nome = variavel
-    valor = float(input('Qual o valor vai retirar para: {}{}{}?: '.format(cores['verde'],nome,cores['fim'])))
+    valor = float(input('\nQual o valor vai retirar para: {}{}{}?: '.format(cores['verde'],nome,cores['fim'])))
     y = x - valor
+    porcentagem = 100 * y / ordenado
     print('Foi retirado: ${}{:.2f}{}, para {}{}{}, restando na carteira o valor de: ${}{:.2f}{}'.format(cores['azul'],valor,cores['fim'],cores['verde'],nome,cores['fim'],cores['azul'],y,cores['fim']))
+    print('A porcentagem da dispesa, {}, é de: {:.2f} %'.format(nome, porcentagem))
 else:
     print('\nok, {}{}{}: temos na carteira valor total de: ${}{:.2f}{}'.format(cores['roxo'],nome,cores['fim'],cores['azul'],x,cores['fim']))
 
