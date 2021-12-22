051caec809ab55b973390ad9d2d7a1776bb98b13
Frederico Hallamman <fredericohallamman@gmail.com>
2021-12-22 23:45:20 +0000

    adicionado loop
    foi adicionado o loop usando o (while)
    22/12/2021

--- carteira GITHUB.py
+++ carteira GITHUB.py
@@ -3,7 +3,7 @@ cores = {'roxo':'\033[1;35m', 'azul':'\033[;34m', 'red':'\033[1;31m','verde':'\0
 print('Hello {}{}{}!!! '.format(cores['roxo'], nome, cores['fim']))
 print('A partir de agora vou te auxiliar na sua gestão financeira ok.')
 
-ordenado = float(input('\nDigite o valor do seu ordenado: '))
+ordenado = float(input('\nDigite o valor do seu Ordenado: '))
 print('\n{}OBS: !!!{}''Em portugal normalmente a conta de luz e gás são juntas e outras separas.'.format(cores['red'], cores['fim']))
 gaseluz = str(input('A sua fatura é junta? s ou n ?: ')).strip()
 
@@ -47,24 +47,24 @@ if internet == 's':
     dispesa = (ordenado - valor - água - net - renda)
     total = dispesa
     porcentagem = 100 * total / ordenado
-    print('\nO valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás, água e internet é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],total,cores['fim']))
+    print('\nO valor do seu Ordenado: ${}{:.2f}{}, menos: luz, gás, água e internet é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],total,cores['fim']))
     print('A porcentagem total das dispesas é de: {:.2f} %'.format(porcentagem))
 else:
     dispesa = (ordenado - valor - água - renda)
     total = dispesa
     porcentagem = 100 * total / ordenado
-    print('O valor do seu ordenado: ${}{:.2f}{}, menos: luz, gás e água, aluguel é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],total,cores['fim']))
+    print('O valor do seu Ordenado: ${}{:.2f}{}, menos: luz, gás e água, aluguel é de: ${}{:.2f}{} '.format(cores['azul'],ordenado,cores['fim'],cores['azul'],total,cores['fim']))
     print('A porcentagem total das dispesas é de: {:.2f} %'.format(porcentagem))
 
 açao = str(input('\nVai querer adicionar mais alguma dispesa? s ou n ?: ')).strip()
-if açao == 's':
+while açao == 's':
     variavel = str(input('\nDigite o nome da da dispesa para registrar: ')).title().strip()
-    nome = variavel
-    valor = float(input('\nQual o valor vai retirar para: {}{}{}?: '.format(cores['verde'],nome,cores['fim'])))
+    valor = float(input('\nQual o valor vai retirar para: {}{}{}?: '.format(cores['verde'],variavel,cores['fim'])))
     valor_da_açao = total - valor
     porcentagem = 100 * valor / ordenado
     print('Foi retirado: ${}{:.2f}{}, para {}{}{}, restando na carteira o valor de: ${}{:.2f}{}'.format(cores['azul'],valor,cores['fim'],cores['verde'],nome,cores['fim'],cores['azul'],valor_da_açao,cores['fim']))
     print('A porcentagem da dispesa, {}, é de: {:.2f} %'.format(nome, porcentagem))
+    açao = str(input('\nVai querer adicionar mais alguma dispesa? s ou n ?: ')).strip()
 else:
     print('\nok, {}{}{}: temos na carteira valor total de: ${}{:.2f}{}'.format(cores['roxo'],nome,cores['fim'],cores['azul'],total,cores['fim']))
 
